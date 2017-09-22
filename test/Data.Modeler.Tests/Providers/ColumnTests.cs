fc13ee3d70a97fc7d507532c1f96148dd7ebb35f
James Craig <none@none>
2017-09-22 10:25:05 -0400

    More tests.

--- test/Data.Modeler.Tests/Providers/ColumnTests.cs
+++ test/Data.Modeler.Tests/Providers/ColumnTests.cs
@@ -1,5 +1,6 @@
 ﻿using Data.Modeler.Providers;
 using Data.Modeler.Tests.BaseClasses;
+using System;
 using Xunit;
 
 namespace Data.Modeler.Tests.Providers
@@ -7,12 +8,75 @@ namespace Data.Modeler.Tests.Providers
     public class ColumnTests : TestingFixture
     {
         [Fact]
+        public void Bool()
+        {
+            var TempColumn = new Column<bool>("A", System.Data.DbType.Int32, 0, true, true, true, true, true, null, null, default(bool), null, true, true, true, null);
+            Assert.True(TempColumn.AutoIncrement);
+            Assert.Equal(System.Data.DbType.Int32, TempColumn.DataType);
+            Assert.Equal("", TempColumn.Default);
+            Assert.Empty(TempColumn.ForeignKey);
+            Assert.True(TempColumn.Index);
+            Assert.Equal(0, TempColumn.Length);
+            Assert.Equal("A", TempColumn.Name);
+            Assert.True(TempColumn.Nullable);
+            Assert.True(TempColumn.OnDeleteCascade);
+            Assert.True(TempColumn.OnDeleteSetNull);
+            Assert.True(TempColumn.OnUpdateCascade);
+            Assert.Null(TempColumn.ParentTable);
+            Assert.True(TempColumn.PrimaryKey);
+            Assert.True(TempColumn.Unique);
+            Assert.Null(TempColumn.ComputedColumnSpecification);
+        }
+
+        [Fact]
         public void Creation()
         {
             var TempColumn = new Column<int>("A", System.Data.DbType.Int32, 0, true, true, true, true, true, null, null, 1, null, true, true, true, null);
             Assert.True(TempColumn.AutoIncrement);
             Assert.Equal(System.Data.DbType.Int32, TempColumn.DataType);
             Assert.Equal("1", TempColumn.Default);
+            Assert.Empty(TempColumn.ForeignKey);
+            Assert.True(TempColumn.Index);
+            Assert.Equal(0, TempColumn.Length);
+            Assert.Equal("A", TempColumn.Name);
+            Assert.True(TempColumn.Nullable);
+            Assert.True(TempColumn.OnDeleteCascade);
+            Assert.True(TempColumn.OnDeleteSetNull);
+            Assert.True(TempColumn.OnUpdateCascade);
+            Assert.Null(TempColumn.ParentTable);
+            Assert.True(TempColumn.PrimaryKey);
+            Assert.True(TempColumn.Unique);
+            Assert.Null(TempColumn.ComputedColumnSpecification);
+        }
+
+        [Fact]
+        public void DateTime()
+        {
+            var TempColumn = new Column<DateTime>("A", System.Data.DbType.Int32, 0, true, true, true, true, true, null, null, default(DateTime), null, true, true, true, null);
+            Assert.True(TempColumn.AutoIncrement);
+            Assert.Equal(System.Data.DbType.Int32, TempColumn.DataType);
+            Assert.Equal("", TempColumn.Default);
+            Assert.Empty(TempColumn.ForeignKey);
+            Assert.True(TempColumn.Index);
+            Assert.Equal(0, TempColumn.Length);
+            Assert.Equal("A", TempColumn.Name);
+            Assert.True(TempColumn.Nullable);
+            Assert.True(TempColumn.OnDeleteCascade);
+            Assert.True(TempColumn.OnDeleteSetNull);
+            Assert.True(TempColumn.OnUpdateCascade);
+            Assert.Null(TempColumn.ParentTable);
+            Assert.True(TempColumn.PrimaryKey);
+            Assert.True(TempColumn.Unique);
+            Assert.Null(TempColumn.ComputedColumnSpecification);
+        }
+
+        [Fact]
+        public void TimeSpan()
+        {
+            var TempColumn = new Column<TimeSpan>("A", System.Data.DbType.Int32, 0, true, true, true, true, true, null, null, default(TimeSpan), null, true, true, true, null);
+            Assert.True(TempColumn.AutoIncrement);
+            Assert.Equal(System.Data.DbType.Int32, TempColumn.DataType);
+            Assert.Equal("", TempColumn.Default);
             Assert.Empty(TempColumn.ForeignKey);
             Assert.True(TempColumn.Index);
             Assert.Equal(0, TempColumn.Length);
--- test/Data.Modeler.Tests/Providers/SQLServer/CommandBuilders/TableCommandBuilderTests.cs
+++ test/Data.Modeler.Tests/Providers/SQLServer/CommandBuilders/TableCommandBuilderTests.cs
@@ -1,5 +1,6 @@
 ﻿using Data.Modeler.Providers.SQLServer.CommandBuilders;
 using Data.Modeler.Tests.BaseClasses;
+using System;
 using System.Linq;
 using Xunit;
 
@@ -22,6 +23,17 @@ namespace Data.Modeler.Tests.Providers.SQLServer.CommandBuilders
             var Commands = TempCheckConstraint.GetCommands(DesiredSource, null).ToList();
             Assert.Equal(2, Commands.Count());
             Assert.Equal("CREATE TABLE [dbo].[Table A]([Column A] Int,[Column B] NVarChar(MAX))", Commands[0]);
+            Assert.Equal("CREATE TABLE [dbo].[Foreign Table]([Foreign Column] Int)", Commands[1]);
+        }
+
+        [Fact]
+        public void GetCommandsNoCurrentSourceWithDefaults()
+        {
+            DesiredSource.Tables.First().AddColumn<TimeSpan>("OtherStuff", System.Data.DbType.DateTime, nullable: false);
+            var TempCheckConstraint = new TableCommandBuilder();
+            var Commands = TempCheckConstraint.GetCommands(DesiredSource, null).ToList();
+            Assert.Equal(2, Commands.Count());
+            Assert.Equal("CREATE TABLE [dbo].[Table A]([Column A] Int,[Column B] NVarChar(MAX),[OtherStuff] DateTime NOT NULL)", Commands[0]);
             Assert.Equal("CREATE TABLE [dbo].[Foreign Table]([Foreign Column] Int)", Commands[1]);
         }
 
